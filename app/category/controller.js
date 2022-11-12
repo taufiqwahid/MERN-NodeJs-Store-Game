624a523a11ef55f4d4200d851eb615feb0bf2c54
taufiqwahid <taufiq.wahid58@gmail.com>
2022-11-12 21:58:59 +0700

    controller, model and router category

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,9 +1,73 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", { title: "Expresssssss js" });
+      let category = await Category.find();
+      console.log("cek datanya category", category);
+
+      res.render("admin/category/view_category", { category });
     } catch (error) {
-      console.log("resss", error);
+      console.log("errrr", error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (error) {
+      console.log("errrr", error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = await Category({
+        name,
+      });
+      category.save();
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log("errrr", error);
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req?.params;
+
+      let category = await Category.findById(id);
+
+      res.render("admin/category/edit", { category });
+    } catch (error) {
+      console.log("errrr", error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { name } = req.body;
+      const { id } = req.params;
+
+      let category = await Category.findByIdAndUpdate(id, {
+        name,
+      });
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log("errrr", error);
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      let category = await Category.findByIdAndDelete(id);
+
+      if (category) {
+        res.redirect("/category");
+      }
+    } catch (error) {
+      console.log("errrr", error);
     }
   },
 };
